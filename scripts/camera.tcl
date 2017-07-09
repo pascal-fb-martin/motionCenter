ad72f9182d20b325ff51194fcc66bb45cbfe67e4
Pascal Martin <pascal.fb.martin@gmail.com>
2017-07-09 11:15:43 -0700

    Display list of detected cameras on the main page, list cameras in the proper order

--- scripts/camera.tcl
+++ scripts/camera.tcl
@@ -2,13 +2,26 @@
 
 Direct_Url /api/camera webApiCamera
 
+proc cameracompare {a b} {
+
+   set la [string length $a]
+   set lb [string length $b]
+
+   if {$la < $lb} {
+      return -1
+   } elseif {$la > $lb} {
+      return 1
+   }
+   return [string compare $a $b]
+}
+
 proc webApiCamera/list {} {
 
    global cameradb
 
    set sep "\["
 
-   foreach camera [lsort [array names cameradb]] {
+   foreach camera [lsort -command cameracompare [array names cameradb]] {
       append result "${sep}{\"name\":\"$camera\",\"url\":\"$cameradb($camera)\"}"
       set sep ","
    }
@@ -21,6 +34,19 @@ proc webApiCamera/list {} {
 #
 proc camera {name url} {
    global cameradb
+
+   # Avoid assigning the same URL to multiple cameras. This can happen
+   # after a camera reboot if the DHCP server assigned the same IP address
+   # as another previously known camera witch lease expired.
+   # (It does not matter that we delete this very camera as well, since
+   # we are immediately re-createing the record.)
+   #
+   foreach known [array names cameradb] {
+      if {$cameradb($known) == $url} {
+         unset cameradb($known)
+      }
+   }
+
    set cameradb($name) $url
 }
 
