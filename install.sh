bd6f9d8ab2e712e17321d1e700915207ee9ff139
Pascal Martin <pascal.fb.martin@gmail.com>
2017-09-09 22:05:23 -0700

    The installation to copy the tclhttpd start script

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 #
 # Crude installation script for motionCenter.
 #
+# This script exists because it is much easier than making a .deb or .rpm,
+# and it should work on most Linux installations.
+#
+
+#  Create the directories.
+
 INSTALLPATH=$1
 if [ $# -lt 1 ] ; then
    INSTALLPATH=/storage/motion
@@ -11,20 +17,42 @@ if [ ! -d $INSTALLPATH ] ; then
    mkdir -p $INSTALLPATH
 fi
 
+for dir in public scripts video config ; do
+   mkdir -p $INSTALLPATH/$dir
+done
+
+
+# Install the software itself.
+
 for dir in public scripts ; do
-  mkdir -p $INSTALLPATH/$dir
   cp -uv $dir/* $INSTALLPATH/$dir
 done
 
-mkdir -p $INSTALLPATH/config
 for file in config/*.rc ; do
   if [ ! -e $INSTALLPATH/$file ] ; then
     cp -v $file $INSTALLPATH/config
   fi
 done
 
-mkdir -p $INSTALLPATH/videos
+
+# Set proper ownership for all these files.
+
+for dir in public scripts video config ; do
+   chown -R motion:motion $INSTALLPATH/$dir
+done
+
+
+# Install the tclHttpd default's configuration.
 
 SYSCONFIG=/etc/default/tclhttpd
 cp -uv $INSTALLPATH/config/tclhttpd.rc $SYSCONFIG
+
+
+# Install the SYS V style init script.
+
+if [ ! -e /etc/init.d/tclhttpd ] ; then
+   cp -uv sysv/tclhttpd.init /etc/init.d
+   chown root:root /etc/init.d/tclhttpd
+   chmod 755 /etc/init.d/tclhttpd
+fi
 
